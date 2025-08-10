95cd836839bb11e92e182f4bb8f51246c02f75cc
Kidrty <wxy@KidrtydeMacBook-Air.local>
2025-08-10 18:13:52 +0800

    updata1

--- mutilThread.py
+++ mutilThread.py
@@ -1,1 +1,20 @@
 import threading
+import os
+import json
+from concurrent.futures import ThreadPoolExecutor
+
+
+
+def process():
+    print("==进程==")
+
+
+if __name__ == "main":
+
+    files = ["test.json", "test2.json"]
+    with ThreadPoolExecutor(max_workers=10) as e:
+        futures = [e.submit(process) for file in files]
+
+    for future in futures:
+        futures.result()
+    
